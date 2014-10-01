1a7729b43dd00c1c91533564ba4058b5c451e9c1
Andrew Zimolzak <andyzimolzak@gmail.com>
2014-10-01 11:42:46 -0400

    Bearing working but broke Distance

--- haversine.py
+++ haversine.py
@@ -4,16 +4,48 @@
 
 import math
 
-def distance(origin, destination):
-    lat1, lon1 = origin
-    lat2, lon2 = destination
-    radius = 6371 # km
+km = 6371.009
+mi = 3958.761
+nm = 3440.07
 
+seattle = [47.621800, -122.350326]
+olympia = [47.041917, -122.893766]
+
+def unpack(origin, destination):
+    if len(origin) == 2:
+        lat_d1, lon_d1 = origin
+    elif len(origin) == 3:
+        pass
+    else:
+        assert 1==0
+    if len(destination) == 2:
+        lat_d2, lon_d2 = destination
+    elif len(destination) == 3:
+        pass
+    else:
+        assert 1==0
+    return [lat_d1, lon_d1, lat_d2, lon_d2]
+
+def distance(origin, destination):
+    [lat1, lon1, lat2, lon2] = unpack(origin, destination)
+    radius = km
     dlat = math.radians(lat2-lat1)
     dlon = math.radians(lon2-lon1)
+    lat1 = math.radians(lat1)
+    lon1 = math.radians(lon1)
+    lat2 = math.radians(lat2)
+    lon2 = math.radians(lon2)
+
     a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(math.radians(lat1)) \
         * math.cos(math.radians(lat2)) * math.sin(dlon/2) * math.sin(dlon/2)
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
     d = radius * c
-
     return d
+
+def ini_bearing(origin, destination):
+    [lat1, lon1, lat2, lon2] = unpack(origin, destination)
+    y = math.sin(lon2-lon1) * math.cos(lat2)
+    x = math.cos(lat1) * math.sin(lat2) - math.sin(lat1) * math.cos(lat2) * math.cos(lon2-lon1)
+    brng = math.atan2(y, x) # radians
+    return brng
+
